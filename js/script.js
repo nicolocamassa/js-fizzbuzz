ce4b1692851d1bd842b53efc05880880079a0f00
nicolocamassa <camassanicolo03@gmail.com>
2023-11-22 14:58:49 +0100

    [ADD] multiples control added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 let i;
 for(i = 1; i <= 100; i++){
+    if(i%5 == 0 && i%3 == 0){
+        console.log(i + ' ciupa');
+    }else if(i%5 == 0){
+        console.log(i + ' multiplo di 5');
+    }
+    else if(i%3 == 0){
+        console.log(i + ' multiplo di 3');
+    }
+
     console.log(i);
 }
