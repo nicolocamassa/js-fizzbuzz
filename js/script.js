9bfe1abb1c2a5fc4ba29dd2c70e2b422b62bdf69
nicolocamassa <camassanicolo03@gmail.com>
2023-11-22 18:00:45 +0100

    [FIX] if else replaced with an else

--- js/script.js
+++ js/script.js
@@ -2,34 +2,27 @@ let i;
 let container = document.querySelector('.container');
 let value;
 
-
-// Append the "li" node to the list:
-
-
 /* Ciclo da 1 a 100 */
-for(i = 1; i <= 100; i++){
-    /* Stampa dei numeri NON multipli di 5 e 3*/
-    if(i%5 != 0 && i%3 != 0){
-        value = i;
-    }else{
-        /* Controllo dei numeri multipli di 5 e 3*/
-        if(i%5 == 0 && i%3 == 0){
-            value = "FuzzBuzz";
+for (i = 1; i <= 100; i++) {
+
+    /* Controllo dei numeri multipli di 5 e 3*/
+    if (i % 5 == 0 && i % 3 == 0) {
+        value = "FizzBuzz";
 
         /* Controllo dei numeri multipli SOLAMENTE di 5*/
-        }else if(i%5 == 0){
-            value = "Buzz";
-        }
-        
+    } else if (i % 5 == 0) {
+        value = "Buzz";
         /* Controllo dei numeri multipli SOLAMENTE di 3*/
-        else if(i%3 == 0){
-            value = "Fuzz";
-        }
+    } else if (i % 3 == 0) {
+        value = "Fizz";
+        /* Stampa dei numeri NON multipli di 5 e 3*/
+    } else {
+        value = i;
     }
 
-        /* Stampa di tutti i numeri in HTML*/
-        const node = document.createElement("p");
-        const textnode = document.createTextNode(value);
-        node.appendChild(textnode);
-        container.appendChild(node);
+    /* Stampa di tutti i numeri in HTML*/
+    const node = document.createElement("p");
+    const textnode = document.createTextNode(value);
+    node.appendChild(textnode);
+    container.appendChild(node);
 }
