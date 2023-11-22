a2ae0505a8a16fc93df26eccece8a51ca132906b
nicolocamassa <camassanicolo03@gmail.com>
2023-11-22 15:26:04 +0100

    [UPDATE] replace multiplier number

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let i;
 let container = document.querySelector('.container');
-
+let value;
 
 
 // Append the "li" node to the list:
@@ -8,28 +8,28 @@ let container = document.querySelector('.container');
 
 /* Ciclo da 1 a 100 */
 for(i = 1; i <= 100; i++){
-    /* Stampa di tutti i numeri in HTML*/
-    const node = document.createElement("p");
-    const textnode = document.createTextNode(i);
-    node.appendChild(textnode);
-    container.appendChild(node);
-
     /* Stampa dei numeri NON multipli di 5 e 3*/
     if(i%5 != 0 && i%3 != 0){
-        console.log(i)
+        value = i;
     }else{
         /* Controllo dei numeri multipli di 5 e 3*/
         if(i%5 == 0 && i%3 == 0){
-            console.log("FuzzBizz");
+            value = "FuzzBuzz";
 
         /* Controllo dei numeri multipli SOLAMENTE di 5*/
         }else if(i%5 == 0){
-            console.log("Buzz");
+            value = "Buzz";
         }
         
         /* Controllo dei numeri multipli SOLAMENTE di 3*/
         else if(i%3 == 0){
-            console.log("Fuzz");
+            value = "Fuzz";
         }
     }
+
+        /* Stampa di tutti i numeri in HTML*/
+        const node = document.createElement("p");
+        const textnode = document.createTextNode(value);
+        node.appendChild(textnode);
+        container.appendChild(node);
 }
